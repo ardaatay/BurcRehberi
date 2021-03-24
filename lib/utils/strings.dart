bdddea9b9ca80fd81d1d27e5de671242f37ffa71
ardaatay <atay_arda@outlook.com>
2021-03-24 14:40:43 +0300

    initial commit

---
+++ lib/utils/strings.dart
@@ -0,0 +1,33 @@
+class Strings {
+  static const String APP_NAME = "Burç Rehberi";
+
+  static const List<String> BURC_ADLARI = [
+    "Koc",
+    "Boga",
+    "Ikizler",
+    "Yengec",
+    "Aslan",
+    "Basak",
+    "Terazi",
+    "Akrep",
+    "Yay",
+    "Oglak",
+    "Kova",
+    "Balik"
+  ];
+
+  static const List<String> BURC_TARIHLERI = [
+    "21 Mart - 20 Nisan",
+    "21 Nisan - 21 Mayıs",
+    "22 Mayıs - 22 Haziran",
+    "23 Haziran - 22 Temmuz",
+    "23 Temmuz - 22 Agustos",
+    "23 Agustos - 22 Eylul",
+    "23 Eylul - 22 Ekım",
+    "23 Ekım - 21 Kasım",
+    "22 Kasım - 21 Aralık",
+    "22 Aralık - 21 Ocak",
+    "22 Ocak - 19 Subat",
+    "20 Subat - 20 Mart"
+  ];
+}
